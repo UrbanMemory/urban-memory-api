0decf38c8b6900369a5c57fbb13aa95a924a1054
Ryan Khalili <Khalili.14@osu.edu>
2023-11-02 16:49:20 -0400

    New Fixes

--- UrbanMemory.Domain/Catelog/Item.cs
+++ UrbanMemory.Domain/Catelog/Item.cs
@@ -1,45 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 namespace UrbanMemory.Domain.Catelog
 {
-    public class Item {
+    public class Item
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Brand { get; set; }
         public decimal Price { get; set; }
         public List<Rating> Ratings { get; set; } = new List<Rating>();
-    }
 
-    public Item(string name, string description, string brand, decimal price)
-    {
-        if (string.IsNullOrEmpty(name))
+        public Item(string name, string description, string brand, decimal price)
         {
-            throw new ArgumentNullException(name);
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentNullException(nameof(description));
+            }
+
+            if (string.IsNullOrEmpty(brand))
+            {
+                throw new ArgumentNullException(nameof(brand));
+            }
+
+            if (price < 0.00m)
+            {
+                throw new ArgumentException("Price must be greater than zero.");
+            }
+
+            Name = name;
+            Description = description;
+            Brand = brand;
+            Price = price;
         }
 
-        if (string.IsNullOrEmpty(description))
+        public void AddRating(Rating rating)
         {
-            throw new ArgumentNullException(description);
+            this.Ratings.Add(rating);
         }
-
-        if (string.IsNullOrEmpty(brand))
-        {
-            throw new ArgumentNullException(brand);
-        }
-
-        if(price < 0.00m)
-        {
-            throw new ArgumentException("Price must be greater than zero.");
-        }
-
-        Name = name;
-        Description = description;
-        Brand = brand;
-        Price = price;
-    }
-    public void AddRating(Rating rating)
-    {
-        this.Ratings.Add(rating);
     }
 }
--- UrbanMemory.Domain/Catelog/Rating.cs
+++ UrbanMemory.Domain/Catelog/Rating.cs
@@ -1,24 +1,25 @@
-namespace UrbanMemory.Domain.Catelog;
-
-public class Rating 
+namespace UrbanMemory.Domain.Catelog
 {
-    public int Stars { get; set; }
-    public string UserName { get; set; }
-    public string Review { get; set; }
-}
-
-public Rating(int stars, string username, string review)
-{
-    if (stars < 1 || stars > 5)
+    public class Rating 
     {
-        throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
-    }
-    if (string.IsNullOrEmpty(username))
-    {
-        throw new ArgumentException("UserName cannot be null.");
-    }
+        public int Stars { get; set; }
+        public string UserName { get; set; }
+        public string Review { get; set; }
 
-    this.Stars = stars;
-    this.UserName = username;
-    this.Review = review;
+        public Rating(int stars, string username, string review)
+        {
+            if (stars < 1 || stars > 5)
+            {
+                throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
+            }
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentException("UserName cannot be null.");
+            }
+
+            this.Stars = stars;
+            this.UserName = username;
+            this.Review = review;
+        }
+    }
 }
