2e08a51501efecbb4ad049e80d74d0f72933b87a
Tyler Yee <yee.s.tyler@gmail.com>
2023-11-28 19:05:07 -0500

    page10

--- UrbanMemory.Api/Program.cs
+++ UrbanMemory.Api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(builder =>
     {
         builder.WithOrigins("http://localhost:3000")
-            .AllowAnyHeader();
+            .AllowAnyHeader()
             .AllowAnyMethod();
     });
 });
---
+++ UrbanMemory.Api/Security/HasScopeHandler.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Emerald.Tiger.Api.Security
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirements>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            HasScopeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+
+            var scopes = context.User
+                .FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer)
+                .Value.Split(' ');
+
+            if (scopes.Any(s => == requirement.Scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
